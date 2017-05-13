a21a2a1b5a0f42e019fe4eec44234b614da61b07
programus <programus@gmail.com>
2017-05-13 16:29:47 +0900

    added sound effects of Japan traffic lights

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ import random
 import signal
 import sys
 import threading
+import os
+import subprocess
 
 RED_PIN = 17
 YELLOW_PIN = 27
@@ -22,6 +24,12 @@ BUTTON_PIN = 4
 
 BUTTON_DELAY_TIME = 5
 
+SOUND_PATH = '../sounds'
+SOUND_PLAYER_TERMIATE_DELAY = 0.3
+
+NS_SOUND = 'NS.wav'
+WE_SOUND = 'WE.wav'
+
 pin_map = {
   'r': RED_PIN,
   'y': YELLOW_PIN,
@@ -31,7 +39,7 @@ pin_map = {
 }
 
 states = [
-  { 'name': 'all-green', 'on_time': 15, 'off_time': 0, 'repeat': 0, 'on_lights': 'gG', 'interruptable': False, 'rand_time': 3, 'enable_button': False },
+  { 'name': 'all-green', 'on_time': 15, 'off_time': 0, 'repeat': 0, 'on_lights': 'gG', 'interruptable': False, 'rand_time': 3, 'enable_button': False, 'sound': NS_SOUND },
   { 'name': 'blink-green', 'on_time': 0.5, 'off_time': 0.5, 'repeat': 5, 'on_lights': 'gG', 'off_lights': 'G', 'interruptable': True, 'rand_time': 0, 'enable_button': True },
   { 'name': 'human-red', 'on_time': 2, 'off_time': 0, 'repeat': 0, 'on_lights': 'gR', 'interruptable': False, 'rand_time': 0, 'enable_button': True },
   { 'name': 'yellow', 'on_time': 1.7, 'off_time': 0, 'repeat': 0, 'on_lights': 'yR', 'interruptable': False, 'rand_time': 0, 'enable_button': True },
@@ -42,6 +50,7 @@ states = [
 curr_state = None
 button_event = threading.Event()
 button_state = 0  # x0b - diabled, x1b - enabled, 1xb - pressed, 0xb - not pressed
+player_process = None
 
 def setup():
   GPIO.setmode(GPIO.BCM)
@@ -111,6 +120,7 @@ def process_state(state):
   curr_state = state
   print state
   enable_button(state.get('enable_button'))
+  play_state_sound(state)
   all_lights, pins = zip(*pin_map.iteritems())
   for _ in xrange(max(1, state['repeat'])):
     # turn on lights
@@ -125,6 +135,24 @@ def process_state(state):
     if state.get('off_time'):
       GPIO.output(map(lambda l: pin_map[l], off_lights), False)
       wait(state, on_time)
+
+def play_state_sound(state):
+  global player_process
+  sound = state.get('sound')
+  if sound:
+    filepath = os.path.realpath(os.path.join(os.path.dirname(__file__), SOUND_PATH, sound))
+    player_process = subprocess.Popen(['omxplayer', '--loop', filepath], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print 'playing %s in process[%d] ...' % (filepath, player_process.pid)
+  else:
+    if player_process:
+      player_process.stdin.write('q')
+      def reset_player():
+        global player_process
+        if player_process.poll() is None:
+          print 'terminate player'
+          player_process.terminate()
+        player_process = None
+      threading.Timer(SOUND_PLAYER_TERMIATE_DELAY, reset_player).start()
 
 def wait(state, duration):
   if state.get('interruptable'):
